2d09b56cbe06e815bd41383398849482180c7df0
ilmenichetti <ilmenichetti@gmail.com>
2022-12-20 14:02:49 +0100

    Initialization completed. I added a function for extending the provided time series to the length of the simulation, which is now run until the final felling. The management is defined by another function, according to standard Swedish managemeng guidelines for clear cuts.

--- pkg/R/Q_ss.R
+++ pkg/R/Q_ss.R
@@ -27,6 +27,8 @@
 #'
 #' @references
 #' Menichetti, L., Mäkinen, H., Stendahl, J., Ågren, G.I., Hyvönen, R., 2021. Modeling persistence of coarse woody debris residuals in boreal forests as an ecological property. Ecosphere 12. https://doi.org/10.1002/ecs2.3792
+#'
+#' @seealso \code{\link{C_init}}
 
 Q_ss= function(fc, inputs, e0, eta11, b, u0, q0, Tmax){
 
@@ -49,7 +51,9 @@ Q_ss= function(fc, inputs, e0, eta11, b, u0, q0, Tmax){
 #'
 #' @references
 #' Menichetti, L., Mäkinen, H., Stendahl, J., Ågren, G.I., Hyvönen, R., 2021. Modeling persistence of coarse woody debris residuals in boreal forests as an ecological property. Ecosphere 12. https://doi.org/10.1002/ecs2.3792
-
+#'
+#' @seealso \code{\link{Q_ss}}
+#'
 
 u0_calc<-function(lat){
   u0_calc=(0.0855+0.0157*(50.6-0.768*lat))
@@ -59,20 +63,24 @@ u0_calc<-function(lat){
 
 
 #' @title Initializing the steady states of the decomposing pools
-#' @description
+#' @description This function utilizes the steady states solutions for the Q decomposition model  \code{\link{Q_ss}} to initialize the decomposing pools assuming that the simulation starts at their steady states, which are determined once the Q model parameters are known and we can assume steady inputs.
+#'  It assumes, with some approximation, as steady inputs of C the average inputs from a whole management cycle.
 #'
-#' @param latitude self explanatory
-#' @param site,
-#' @param species,
-#' @param climate,
-#' @param thinning,
-#' @param parameters,
-#' @param parsQlitter,
-#' @param soil,
-#' @param size_dist,
-#' @param settings
+#' @param latitude self explanatory, it is used to initialize the Q model throught the function  refer to \code{\link{u0_calc}}
+#' @param site refer to \code{\link{run_3PG}}
+#' @param species  refer to \code{\link{run_3PG}}
+#' @param climate  refer to \code{\link{run_3PG}}
+#' @param thinning  refer to \code{\link{run_3PG}}
+#' @param parameters  refer to \code{\link{run_3PG}}
+#' @param parsQlitter  refer to \code{\link{run_3PG}}
+#' @param soil  refer to \code{\link{run_3PG}}
+#' @param size_dist  refer to \code{\link{run_3PG}}
+#' @param settings  refer to \code{\link{run_3PG}}
 #'
-#' @details
+#' @details the function assumes the management plan from the function \code{\link{management_plan}}, which contains the references for standard Swedish clear-cut forestry practices
+#' The function considers as simulation length the whole cycle indicated by \code{\link{management_plan}} plus one year, and utilizes the function \code{\link{extend_time}} to repeat the provided climatic time series over the needed time.
+#'
+#' @seealso \code{\link{management_plan}}, \code{\link{extend_time}}, \code{\link{u0_calc}}, , \code{\link{Q_ss}}
 
 C_init<-function(lat,
                  site,
@@ -85,20 +93,62 @@ C_init<-function(lat,
                  settings = list(light_model = 2, transp_model = 2, phys_model = 2,
                                  height_model = 1, correct_bias = 0, calculate_d13c = 0)){
 
-  soilInit<-mat.or.vec(dim(my_species)[1], 5)
-  for(i in 1:dim(soilInit)[1]){
-    soilInit[i,]<-c(3,3,3,3,0)
+
+  management<-management_plan(SI,lat)
+  thinning_boolean<-management[8:11]!=1
+  management[9]<-management[8]*management[9]
+  management[10]<-management[9]*management[10]
+  management[11]<-management[10]*management[11]
+
+  main_species<-species[which.max(species$stems_n),]
+  main_species$stems_n<-management$N
+  main_thinning<-thinning[0,]
+  for(i in 1:length(which(thinning_boolean))){
+    if(thinning_boolean[i]==T){
+      main_thinning_line<-thinning[thinning$species==as.character(main_species[1]),][1,]
+      main_thinning_line$age=management[2+i]
+      main_thinning_line$stems_n=round(management[7+i]*management$N)
+      main_thinning_line$stem=1
+      main_thinning_line$root=1
+      main_thinning_line$foliage=1
+      main_thinning<-rbind(main_thinning, main_thinning_line)
+    }
   }
+
+  #final felling
+  main_thinning_last_line<-thinning[thinning$species==as.character(main_species[1]),][1,]
+  main_thinning_last_line$age<-management$gm
+  main_thinning_last_line$stems_n<-0 #no stems are left
+  main_thinning_last_line$stem=1
+  main_thinning_last_line$root=1
+  main_thinning_last_line$foliage=1
+  main_thinning<-rbind(main_thinning, main_thinning_last_line)
+
+
+  #selecting one dummy initialization value for simulating the first time series (to calculate the average inputs)
+  soilInit<-c(3,3,3,3,0)
+
+
+  #extending the cycle to one full length (management$gm+1 length, final felling)
+  main_site<-site
+  main_site$from<-main_species$planted
+  end_cycle<-as.Date(paste(main_site$from, "-01", sep=""), format = "%Y-%m-%d") %m+% years(management$gm+1)
+  main_site$to<-format(end_cycle, "%Y-%m")
+  main_thinning$age<-thinning[2:4,]$age
+
+
+  #extending the climate time series
+  climate_long<-extend_time(climate=climate,from=main_site$from, length=management$gm+1)
 
 
   init_out = run_3PG(
-    site        = site,
-    species     = species,
-    climate     = climate,
-    thinning    = thinning,
+    site        = main_site,
+    species     = main_species,
+    climate     = climate_long,
+    thinning    = main_thinning,
     parameters  = parameters,
     parsQlitter = parsQlitter,
-    soil        = soilInit,
+    soil        = my_soilInit[1,],
     size_dist   = size_dist,
     settings    = settings,
     check_input = TRUE, df_out = F)
@@ -107,7 +157,7 @@ C_init<-function(lat,
   Qpars<-as.data.frame(t(my_parsQlitter)[-1,])
   colnames(Qpars)<-(t(my_parsQlitter)[1,])
 
-  for(i in 1:dim(soilInit)[1]){
+  for(i in 1:dim(soilInit_out)[1]){
     soilInit_out[i,1]<-Q_ss(b=as.numeric(Qpars[i,]$beta_fol),
                             e0=as.numeric(Qpars[i,]$e0_fol),
                             q0=as.numeric(Qpars[i,]$q0_fol),
@@ -132,8 +182,8 @@ C_init<-function(lat,
                             u0=u0_calc(lat),
                             fc=as.numeric(Qpars[i,]$fc_stem),
                             eta11=as.numeric(Qpars[i,]$eta_11_stem),
-                            Tmax=mean(init_out[,i,2,5]),
-                            inputs=mean(init_out[,i,4,12]))
+                            Tmax=mean(init_out[,1,2,5]),
+                            inputs=mean(init_out[,1,4,12]))
 
     soilInit_out[i,4]<-Q_ss(b=as.numeric(Qpars[i,]$beta_bran),
                             e0=as.numeric(Qpars[i,]$e0_bran),
@@ -142,7 +192,7 @@ C_init<-function(lat,
                             fc=as.numeric(Qpars[i,]$fc_bran),
                             eta11=as.numeric(Qpars[i,]$eta_11_bran),
                             Tmax=3,
-                            inputs=mean(init_out[,i,4,13]))
+                            inputs=mean(init_out[,1,4,13]))
 
     soilInit_out[i,5]<-0
 
@@ -152,3 +202,110 @@ C_init<-function(lat,
 
 }
 
+
+
+
+
+#main function for management plan selection
+#' @title The function for determining the appropriate management plan.
+#'
+#' @description
+#' The management.plan functions contains a series of vectors with management data from Sweden.
+#' It selects the most appropriate values based on site index and latitude, and determines the
+#' values for the management plan.
+#'
+#' @param SI The site index.
+#' @param Lat The latitude of the simulated site.
+#'
+#' @return a data frame with :
+#' @H0 the start age, when the planted trees are 1.3 m
+#' @N the density of the initial plantation
+#' @g a series of numbers showing at which age the stand is thinned, including the last final felling
+#' @s what ratio is left at the current stand at each thinning
+#'
+#' @seealso \code{\link{C_init}}
+
+management_plan<-function(SI, Lat){
+
+  ### Self-contained declaration of matrices for the management plans
+  # this is in order to skip the reading of the specific csv and make the function more general
+
+  N0<-c(1250,1250,1250,1250,1400,1600,1800,2000,2200,2300,2500,2700,2900,3000)
+
+  gm<-c(141, 141, 141, 141, 126, 121, 111, 106, 101, 95, 85, 80, 75, 70)
+
+  s1_vec<-c(0.75, 0.75, 0.75, 0.75, 0.72, 0.69, 0.71, 0.73, 0.76, 0.75, 0.72, 0.73, 0.76, 0.76)
+  s2_vec<-c(0.71, 0.71, 0.71, 0.71, 0.74, 0.72, 0.73, 0.73, 0.73, 0.74, 0.73, 0.67, 0.71, 0.69)
+  s3_vec<-c(1, 1, 1, 1, 1, 0.86, 0.84, 0.71, 0.74, 0.74, 0.76, 0.72, 0.74, 0.74)
+  s4_vec<-c(1, 1, 1, 1, 1, 1, 1, 1, 1, 0.75, 0.72, 0.76, 0.77, 0.74)
+  s<-as.data.frame(cbind(s1_vec,s2_vec,s3_vec,s4_vec))
+
+  g1_vec<-c(75, 75, 75, 75, 62, 55, 47, 37, 33, 30, 27, 25, 24, 21)
+  g2_vec<-c(100, 100, 100, 100, 82, 76, 65, 53, 48, 43, 36, 35, 35, 30)
+  g3_vec<-c(140, 140, 140, 140, 125, 91, 85, 71, 65, 55, 45, 46, 45, 39)
+  g4_vec<-c(140, 140, 140, 140, 125, 120, 110, 105, 100, 70, 57, 59, 57, 47)
+  g<-as.data.frame(cbind(g1_vec,g2_vec,g3_vec,g4_vec))
+
+  H0<-c(12, 12, 12, 12, 12, 12, 12, 11, 11, 11, 11, 11, 11, 11)
+  gm<-c(141, 141, 141, 141, 126, 121, 111, 106, 101, 95, 85, 80, 75, 70)
+
+
+  #selection id
+  ID<-((SI-10)/2)+1
+
+  ### Select the values from the specific site
+
+  #Year when start diameter at breast hight is measured
+  T.=H0[ID]#was T
+
+
+  #Year of final felling
+  gm=gm[ID]
+  #Thinnings
+  g1=as.numeric(g[ID,1])
+  g2=as.numeric(g[ID,2])
+  g3=as.numeric(g[ID,3])
+  g4=as.numeric(g[ID,4])
+  s1=as.numeric(s[ID,1])
+  s2=as.numeric(s[ID,2])
+  s3=as.numeric(s[ID,3])
+  s4=as.numeric(s[ID,4])
+
+  N=N0[ID]
+
+  thinnings<-data.frame(g1, g2, g3, g4, gm, s1, s2, s3, s4)
+  results<-data.frame(H0=T., N, thinnings)
+
+  return(results)
+}
+
+
+
+
+# function to repeat the climate time series
+#' @title This function repeats the climate time series for a specified length of time. It is used for initialization.
+#'
+#' @description
+#'
+#' @param climate the original climatic time series.
+#' @param from the start date of the desired new time series
+#' @param length length in years of the desired new time series.
+#'
+#' @seealso \code{\link{C_init}}
+
+extend_time<-function(climate, from, length){
+
+  require(lubridate)
+  length<-length+1
+
+  from_date<-as.Date(paste(from, "-01", sep=""), format = "%Y-%m-%d")
+  seq_date<-seq(from_date, by="month", length.out=length*12)
+
+  repetitions<-ceiling(length/(diff(range(climate$year))+1))
+  climate_long<-do.call(rbind, replicate(repetitions, climate, simplify=FALSE)) # where m is your matrix
+  climate_long_cut<-climate_long[1:(length*12),]
+  climate_long_cut$year<-year(seq_date)
+  climate_long_cut$month<-month(seq_date)
+  return(climate_long_cut)
+
+}

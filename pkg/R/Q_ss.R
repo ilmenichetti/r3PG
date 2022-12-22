4ae8d751679a3b314257487a46ce6e15a17d9c5f
ilmenichetti <ilmenichetti@gmail.com>
2022-12-22 14:32:52 +0100

    Updated initialization function, it is now more generic (can accept user iput, but also a default PIcea abies simulation).

--- pkg/R/Q_ss.R
+++ pkg/R/Q_ss.R
@@ -64,18 +64,20 @@ u0_calc<-function(lat){
 
 #' @title Initializing the steady states of the decomposing pools (based on a simulated Picea abies stand)
 #' @description This function utilizes the steady states solutions for the Q decomposition model  \code{\link{Q_ss}} to initialize the decomposing pools assuming that the simulation starts at their steady states, which are determined once the Q model parameters are known and we can assume steady inputs.
-#'  It assumes, with some approximation, as steady inputs of C the average inputs from a whole management cycle.
+#'  The function assumes, with some approximation, as steady inputs of C the average inputs from a whole management cycle.
 #'  When initializing multiple layers, the function assigns the initialized C masses calculated from the initialization simulation proportionally to the number of stems of each layer.
+#'
+#' If a species and thinning plan is not supplied (default), the function assumes a standard swedish Picea Abies plantation with the functions \code{\link{management_plan}} and \code{\link{sim_thinning}}.
+#' Otherwise the function assumes a monoculture with the most represented species (but redistributes the initialization in two pools proportional to the number of planted trees per each species). In that case you need to make sure to specify a whole cultural cycle in your thinning matrix!
 #'
 #' @param latitude self explanatory, it is used to initialize the Q model throught the function  refer to \code{\link{u0_calc}}
 #' @param SI the site index
 #' @param site refer to \code{\link{run_3PG}}
-#' @param species  refer to \code{\link{run_3PG}}
+#' @param species  refer to \code{\link{run_3PG}}, default is NULL
 #' @param climate  refer to \code{\link{run_3PG}}
-#' @param thinning  refer to \code{\link{run_3PG}}
-#' @param parameters  refer to \code{\link{run_3PG}}
-#' @param parsQlitter  refer to \code{\link{run_3PG}}
-#' @param soil  refer to \code{\link{run_3PG}}
+#' @param thinning  refer to \code{\link{run_3PG}}, default is NULL
+#' @param parameters  refer to \code{\link{run_3PG}}, default is NULL
+#' @param parsQlitter  refer to \code{\link{run_3PG}}, default is NULL
 #' @param size_dist  refer to \code{\link{run_3PG}}
 #' @param settings  refer to \code{\link{run_3PG}}
 #'
@@ -83,78 +85,158 @@ u0_calc<-function(lat){
 #' The function considers as simulation length the whole cycle indicated by \code{\link{management_plan}} plus one year, and utilizes the function \code{\link{extend_time}} to repeat the provided climatic time series over the needed time.
 #' Because of this, the function assumes a pure stand of Picea abies as former management.
 #'
-#' @seealso \code{\link{management_plan}}, \code{\link{extend_time}}, \code{\link{u0_calc}}, , \code{\link{Q_ss}}
-
+#' @example example_species<-tibble(species=("Pinus sylvestris"),
+#'  planted=("1955-01"),
+#'  fertility=(0.5),
+#'  stems_n=(2000),
+#'  biom_stem=(90),
+#'  biom_root=(10),
+#'  biom_foliage=(5))
+#'
+#' example_thinning<-tibble(species=c("Pinus sylvestris","Pinus sylvestris"),
+#'                          age=c(50, 75),
+#'                          stems_n=c(1500, 1000),
+#'                          stem=c(1,1),
+#'                          root=c(1,1),
+#'                          foliage=c(1,1))
+#'
+#' example_site<-tibble(latitude   =(47.6),
+#'                      altitude   =(100),
+#'                      soil_class =(3),
+#'                      asw_i      =(999),
+#'                      asw_min    =(0),
+#'                      asw_max    =(100),
+#'                      from       =NA,
+#'                      to         =NA)
+#'
+#' C_init(lat=45,
+#'        SI=23,
+#'        site        = example_site,
+#'        species     = example_species,
+#'        climate     = my_climate,
+#'        thinning    = example_thinning,
+#'        parameters  = my_parameters,
+#'        parsQlitter = my_parsQlitter,
+#'        size_dist   = my_sizeDist,
+#'        settings    = list(light_model = 2, transp_model = 2, phys_model = 2,
+#'                           height_model = 1, correct_bias = 0, calculate_d13c = 0))
+#'
+#' @seealso \code{\link{management_plan}}, \code{\link{sim_thinning}}, \code{\link{extend_time}}, \code{\link{u0_calc}}, , \code{\link{Q_ss}}
 
 C_init<-function(lat,
                  SI,
                  site,
-                 species,
+                 species=NULL,
                  climate,
-                 thinning,
-                 parameters,
-                 parsQlitter,
+                 thinning=NULL,
+                 parameters=NULL,
+                 parsQlitter=NULL,
                  size_dist,
                  settings = list(light_model = 2, transp_model = 2, phys_model = 2,
                                  height_model = 1, correct_bias = 0, calculate_d13c = 0)){
 
-  #simulating a standard management plan (Picea abies)
-  management<-management_plan(SI,lat)
-  thinning_boolean<-management[8:11]!=1
-  management[9]<-management[8]*management[9]
-  management[10]<-management[9]*management[10]
-  management[11]<-management[10]*management[11]
+  planted="1950-01"
 
-  main_species<-species[which.max(species$stems_n),] #legacy, when the simulation was based on the most represented species. It is there to initialize the matrix
-  main_species$species<- "Picea abies"
-  main_species$stems_n<-management$N
-  main_thinning<-thinning[0,]
-  for(i in 1:length(which(thinning_boolean))){
-    if(thinning_boolean[i]==T){
-      main_thinning_line<-thinning[1,][1,]
-      main_thinning_line$species<- "Picea abies"
-      main_thinning_line$age=management[2+i]
-      main_thinning_line$stems_n=round(management[7+i]*management$N)
-      main_thinning_line$stem=1
-      main_thinning_line$root=1
-      main_thinning_line$foliage=1
-      main_thinning<-rbind(main_thinning, main_thinning_line)
-    }
+  #some data check
+  if(is.null(species)){warning("you did not supply a species matrix, assuming plantation date as 1950. This will not influence the final result")}
+  if(is.null(thinning) & !is.null(species)){stop("you supplied a species matrix, you need to supply also the associated thinning matrix")}
+  if(is.null(species) & !is.null(thinning)){stop("you supplied a thinning matrix, you need to supply also the associated species matrix")}
+  if(is.null(thinning) & !is.null(species)){warning("you supplied a species matrix and the associated thinning matrix, these will be used in the initialization simulation. Please make sure that your thinning plan includes a whole cultural cycle")}
+
+
+  if(is.null(species)){
+    sim_management<-sim_thinning(SI, lat, planted)
+    main_species<-sim_management$main_species
+  } else {
+    main_species<-species[which.max(species$stems_n),]
+
   }
 
-  #adding the final felling to the management plan
-  main_thinning_last_line<-thinning[thinning$species==as.character(main_species[1]),][1,]
-  main_thinning_last_line$species<- "Picea abies"
-  main_thinning_last_line$age<-management$gm
-  main_thinning_last_line$stems_n<-0 #no stems are left
-  main_thinning_last_line$stem=1
-  main_thinning_last_line$root=1
-  main_thinning_last_line$foliage=1
-  main_thinning<-rbind(main_thinning, main_thinning_last_line)
+  if(is.null(thinning)){
+    sim_management<-sim_thinning(SI=23, lat=45, planted)
+    main_thinning<-sim_management$main_thinning
+  } else {
+    main_thinning<-thinning
+  }
 
 
   #selecting one dummy initialization value for simulating the first time series (to calculate the average inputs)
   soilInit<-c(3,3,3,3,0)
 
-
   #extending the cycle to one full length (management$gm+1 length, final felling)
   main_site<-site
   main_site$from<-main_species$planted
-  end_cycle<-as.Date(paste(main_site$from, "-01", sep=""), format = "%Y-%m-%d") %m+% years(management$gm+1)
+  end_cycle<-as.Date(paste(main_site$from, "-01", sep=""), format = "%Y-%m-%d") %m+% years(main_thinning[nrow(main_thinning),]$age+1)
   main_site$to<-format(end_cycle, "%Y-%m")
 
-
   #extending the climate time series
-  climate_long<-extend_time(climate=climate,from=main_site$from, length=management$gm+1)
+  climate_long<-extend_time(climate=climate,from=main_site$from, length=as.numeric(main_thinning[nrow(main_thinning),]$age+1))
 
-  #the initialization assumes spruce parameterization
-  Qpars<- list(7.00, 0.36, 0.25, 0.50, 0.00, 1.10, 0.50, 7.00, 0.36,
-               0.25, 0.50, 0.00, 1.10, 0.50, 7.00, 0.36, 0.25, 0.50,
-               0.00, 1.10, 0.50, 7.00, 0.36, 0.25, 0.50, 0.00, 1.10, 0.30)
-  names(Qpars)<-(t(my_parsQlitter)[1,])
+  #if the Q parameters are null, then the initialization assumes spruce parameterization
+  if(is.null(parsQlitter)){
+    Qpars<- list(7.00, 0.36, 0.25, 0.50, 0.00, 1.10, 0.50, 7.00, 0.36,
+                 0.25, 0.50, 0.00, 1.10, 0.50, 7.00, 0.36, 0.25, 0.50,
+                 0.00, 1.10, 0.50, 7.00, 0.36, 0.25, 0.50, 0.00, 1.10, 0.30)
+    names(Qpars)<-c("beta_fol",    "eta_11_fol",  "e0_fol",      "fc_fol",      "delay_fol",   "q0_fol",
+                    "z_leaves",    "beta_root",   "eta_11_root", "e0_root",     "fc_root",     "delay_root",
+                    "q0_root",     "z_root",      "beta_bran",   "eta_11_bran", "e0_bran",     "fc_bran",
+                    "delay_bran",  "q0_bran",     "z_branch",    "beta_stem",   "eta_11_stem", "e0_stem",
+                    "fc_stem",     "delay_stem",  "q0_stem",     "z_stem"    )
 
-  my_parsQlitter<-data.frame(parameter=names(Qpars), "Picea abies"=as.vector(unlist(Qpars)))
-  colnames(my_parsQlitter)[2]="Picea abies"
+    main_parsQlitter<-data.frame(parameter=names(Qpars), "Picea abies"=as.vector(unlist(Qpars)))
+    colnames(main_parsQlitter)[2]="Picea abies"
+  } else {
+    main_parsQlitter<-data.frame( parsQlitter[,1], parsQlitter[,colnames(parsQlitter)==main_species$species])
+    colnames(main_parsQlitter)[2]=main_species$species
+
+    Qpars<-as.list(unlist(parsQlitter[,colnames(parsQlitter)==main_species$species]))
+    names(Qpars)<-c("beta_fol",    "eta_11_fol",  "e0_fol",      "fc_fol",      "delay_fol",   "q0_fol",
+                    "z_leaves",    "beta_root",   "eta_11_root", "e0_root",     "fc_root",     "delay_root",
+                    "q0_root",     "z_root",      "beta_bran",   "eta_11_bran", "e0_bran",     "fc_bran",
+                    "delay_bran",  "q0_bran",     "z_branch",    "beta_stem",   "eta_11_stem", "e0_stem",
+                    "fc_stem",     "delay_stem",  "q0_stem",     "z_stem"    )
+
+  }
+
+
+
+  #if the 3PG parameters are null, then the initialization assumes spruce parameterization
+  if(is.null(parameters)){
+    pars<- list(0.829700,   0.148000,   0.133000,   2.305000,   0.715100,   0.082400,   0.000800,   0.001000,
+                60.000000,  0.000000,   0.000000,   0.000000,   2.472700,  24.949400,  30.137800,   1.000000,
+                0.700000,   9.000000,   1.004000,   0.958400,   0.000000,   0.600000,   1.000000, 400.000000,
+                4.000000,   0.950000,   0.000000,   0.000000,   0.000000,   1.000000, 376.084300,   1.874000,
+                0.488000,   0.436000,   0.437000,   8.710000,   3.850000,  25.100000,   0.637800,   3.000000,
+                0.223700,   3.000000,   5.000000,   0.027000,   0.470000,   0.000000,   0.024600,   3.330000,
+                0.089600,   0.200000,   0.660000,   2.000000,   4.400000,  27.000000,   0.000000,   0.000000,
+                0.000000,   0.400000,   0.400000,   1.000000,   3.000000,  37.730000,  17.850000,   0.006360,
+                0.000115,   2.310000,   0.330000,   0.000000,   0.630000,   0.640000,   0.000000,  -0.069000,
+                0.067000,  35.180000,  27.180000,   0.000000,  -0.005000,   0.000000, -90.000000,   0.800000,
+                24.000000,   2.300000)
+    names(pars)<-c("pFS2" ,         "pFS20",         "aWS",           "nWS",           "pRx",
+                   "pRn",           "gammaF1",       "gammaF0",       "tgammaF",       "gammaR",
+                   "leafgrow",      "leaffall",      "Tmin",          "Topt",          "Tmax",
+                   "kF",            "SWconst",       "SWpower",       "fCalpha700",    "fCg700",
+                   "m0",            "fN0",           "fNn",           "MaxAge",        "nAge",
+                   "rAge",          "gammaN1",       "gammaN0",       "tgammaN",       "ngammaN",
+                   "wSx1000",       "thinPower",     "mF",            "mR",            "mS",
+                   "SLA0",          "SLA1",          "tSLA",          "k",             "fullCanAge",
+                   "MaxIntcptn",    "LAImaxIntcptn", "cVPD",          "alphaCx",       "Y",
+                   "MinCond",       "MaxCond",       "LAIgcx",        "CoeffCond",     "BLcond",
+                   "RGcGw",         "D13CTissueDif", "aFracDiffu",    "bFracRubi",     "fracBB0",
+                   "fracBB1",       "tBB",           "rhoMin",        "rhoMax",        "tRho",
+                   "crownshape",    "aH",            "nHB",           "nHC",           "aV",
+                   "nVB",           "nVH",           "nVBH",          "aK",            "nKB",
+                   "nKH",           "nKC",           "nKrh",          "aHL",           "nHLB",
+                   "nHLL",          "nHLC",          "nHLrh",         "Qa",            "Qb",
+                   "gDM_mol",       "molPAR_MJ"    )
+
+    my_parameters<-data.frame(parameter=names(pars), "Picea abies"=as.vector(unlist(pars)))
+    colnames(my_parameters)[2]="Picea abies"
+    parameters=my_parameters
+  }
+
+
 
   init_out = run_3PG(
     site        = main_site,
@@ -162,7 +244,7 @@ C_init<-function(lat,
     climate     = climate_long,
     thinning    = main_thinning,
     parameters  = parameters,
-    parsQlitter = my_parsQlitter,
+    parsQlitter = main_parsQlitter,
     soil        = soilInit,
     size_dist   = size_dist,
     settings    = settings,
@@ -208,23 +290,24 @@ C_init<-function(lat,
                         Tmax=3,
                         inputs=mean(init_out[,1,4,13], na.rm = T))
 
-  #this pool is not in use in the current model version
   soilInit_out[5]<-0
 
 
+  if(is.null(species)){
+    return(soilInit_out)
+  } else {
 
-  #calculating the proportions to distribute the initialization
-  species_proportions<-species$stems_n/sum(species$stems_n)
-  species_soilInit_out<-mat.or.vec(dim(species)[1], 5)
+    #calculating the proportions to distribute the initialization
+    species_proportions<-species$stems_n/sum(species$stems_n)
+    species_soilInit_out<-mat.or.vec(dim(species)[1], 5)
 
-  for(i in 1:dim(species_soilInit_out)[1]){
-    species_soilInit_out[i,]<-as.numeric(species_proportions[i]*soilInit_out)
+    for(i in 1:dim(species_soilInit_out)[1]){
+      species_soilInit_out[i,]<-as.numeric(species_proportions[i]*soilInit_out)
+    }
+    return(species_soilInit_out)
   }
 
-  return(species_soilInit_out)
-
 }
-
 
 
 
@@ -330,3 +413,75 @@ extend_time<-function(climate, from, length){
   return(climate_long_cut)
 
 }
+
+
+
+
+# function to simulate a plant for thinnings in Picea abies
+#' @title This function simulates a series of thinnings for Picea abies based on \code{\link{management_plan}}
+#'
+#' @param SI The site index.
+#' @param Lat The latitude of the simulated site.
+#' @param planted the date (year and month in format YY-M, as it is specified in \code{\link{run_3PG}})
+#'
+#' @seealso \code{\link{C_init}}, \code{\link{management_plan}}
+
+sim_thinning<-function(SI, lat, planted){
+
+
+  management<-management_plan(SI,lat)
+  thinning_boolean<-management[8:11]!=1
+  management[9]<-management[8]*management[9]
+  management[10]<-management[9]*management[10]
+  management[11]<-management[10]*management[11]
+
+
+  main_species<-tibble(species=("a"),
+                       planted=("a"),
+                       fertility=(1),
+                       stems_n=(1),
+                       biom_stem=(90),
+                       biom_root=(10),
+                       biom_foliage=(5))
+  main_species$species<- "Picea abies"
+  main_species$stems_n<-management$N
+  main_species$planted<-planted
+  main_species$species<- "Picea abies"
+
+  thinning_template<-tibble(species=("a"),
+                            age=(0),
+                            stems_n=(0),
+                            stem=(1),
+                            root=(1),
+                            foliage=(1))
+
+  main_thinning<-thinning_template[0,]
+  for(i in 1:length(which(thinning_boolean))){
+    if(thinning_boolean[i]==T){
+      main_thinning_line<-thinning_template
+      main_thinning_line$species<- "Picea abies"
+      main_thinning_line$age=management[2+i]
+      main_thinning_line$stems_n=round(management[7+i]*management$N)
+      main_thinning_line$stem=1
+      main_thinning_line$root=1
+      main_thinning_line$foliage=1
+      main_thinning<-rbind(main_thinning, main_thinning_line)
+    }
+  }
+
+
+  #final felling
+  main_thinning_last_line<-thinning_template
+  main_thinning_last_line$species<- "Picea abies"
+  main_thinning_last_line$age<-management$gm
+  main_thinning_last_line$stems_n<-0 #no stems are left
+  main_thinning_last_line$stem=1
+  main_thinning_last_line$root=1
+  main_thinning_last_line$foliage=1
+  main_thinning<-rbind(main_thinning, main_thinning_last_line)
+
+
+  return(list(main_species=main_species, main_thinning=main_thinning))
+
+}
+
